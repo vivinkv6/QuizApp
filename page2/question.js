a26450c3cc88bab4e309f4fd7472cb83ccfcae90
vivinkv6 <vivinkv0@gmail.com>
2022-04-12 08:25:08 +0530

    create 10 questions in question.js

---
+++ page2/question.js
@@ -0,0 +1,72 @@
+const Question = [
+    {
+        question: "Inside which HTML element do we put the javacsript ?",
+        option1: "<scripting>",
+        option2: "<js>",
+        option3: "<script>",
+        answer: "<script>"
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'abc.js' ?",
+        option1: "<script href='abc.js'>",
+        option2: "<script name='abc.js'>",
+        option3: "<script src='abc.js'>",
+        answer: "<script src='abc.js'>"
+    },
+    {
+        question: "how do you write 'Hello World' in an alertbox ?",
+        option1: "alertBox('Hello World')",
+        option2: "alert('Hello World')",
+        option3: "msg('hello World')",
+        answer: "alert('Hello World')"
+    },
+    {
+        question: "How do you create a function in javascript ?",
+        option1: "function myfunction()",
+        option2: "function:myfunction()",
+        option3: "function=myfunction()",
+        answer: "function myfunction()"
+    },
+    {
+        question: "How do you call a function named 'myfunction' ?",
+        option1: "myfunction()",
+        option2: "call myfunction()",
+        option3: "call function myfunction()",
+        answer: "myfunction()"
+    },
+    {
+        question: "How to write IF statement in javascript ?",
+        option1: "if i=5 then",
+        option2: "if(i==5)",
+        option3: "if i=5",
+        answer: "if(i==5)"
+    },
+    {
+        question: "How can you add a comment  in a javascript ?",
+        option1: "'This is a comment'",
+        option2: "//This is a comment",
+        option3: "<!--This is a comment-->",
+        answer: "//This is a comment"
+    }, 
+    {
+        question: "How do you round the number 7.25, to the nearest integer ?",
+        option1: "round(7.25)",
+        option2: "Math.round(7.25)",
+        option3: "rnd(7.25)",
+        answer: "Math.round(7.25)"
+    },
+    {
+        question: "How do you find the number with the highest value of x and y ?",
+        option1: "ceil(x,y)",
+        option2: "Math.max(x,y)",
+        option3: "Math.ceil(x,y)",
+        answer: "Math.max(x,y)"
+    },
+    {
+        question: "which event occurs when the user clicks on an HTML element ?",
+        option1: "onclick",
+        option2: "onmouseclick",
+        option3: "onchange",
+        answer: "onclick"
+    },
+]

36236f9144be379a9c132a53b4da994d9f9a0a91
vivinkv6 <vivinkv0@gmail.com>
2022-04-23 09:48:15 +0530

    modify

--- page1/page1.js
+++ page1/page1.js
@@ -1,18 +1,26 @@
 document.querySelector(".start-quiz").style.display="none";
-  
+document.querySelector(".msg-success").style.visibility="hidden";
+document.querySelector(".msg-username").style.visibility="hidden";
+document.querySelector(".msg-useremail").style.visibility="hidden";
  function storeData(){
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     const data={name,email};
     if(name.length < 3 || name.length == ""){
-alert("Please Enter valid UserName")
+        document.querySelector(".msg-useremail").style.visibility="hidden";
+        document.querySelector(".msg-success").style.visibility="hidden";
+        document.querySelector(".msg-username").style.visibility="visible";
     }
     else if(email.length <3 || email.length == ""){
-        alert("Please Enter valid UserEmail")
+        document.querySelector(".msg-success").style.visibility="hidden";
+        document.querySelector(".msg-username").style.visibility="hidden";
+        document.querySelector(".msg-useremail").style.visibility="visible";
     }
     else{
     console.log(data);
-    alert("succesfully Registered");
+    document.querySelector(".msg-username").style.visibility="hidden";
+    document.querySelector(".msg-success").style.visibility="visible";
+    document.querySelector(".msg-useremail").style.visibility="hidden";
     document.querySelector(".register").style.display="none";
     document.querySelector(".start-quiz").style.display="inline";
 }
